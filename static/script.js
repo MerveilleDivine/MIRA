484a85340c67f7487a1bbd85f0189f340be20c07
vinemuganguzi <mervinemuganguao@gmail.com>
2024-02-27 11:20:30 +0200

    images added

--- static/script.js
+++ static/script.js
@@ -1,8 +1,8 @@
 const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
-const themeButton = document.querySelector("#theme-btn");
-const deleteButton = document.querySelector("#delete-btn");
+const themeButton = document.querySelector("#send-btn");
+const deleteButton = document.querySelector("#mic-btn");
 
 let userText = null;
 const API_KEY = "PASTE-YOUR-API-KEY-HERE"; // Paste your API key here
